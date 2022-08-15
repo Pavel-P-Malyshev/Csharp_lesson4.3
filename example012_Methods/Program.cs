291e0f0a7da307352e6aac8033823d615a03581c
Pavel <m83819@gmail.com>
2022-08-15 17:17:57 +0700

    example012_Methods

---
+++ example012_Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿string Replace(string text, char oldval, char newval)
+{
+    string result=String.Empty;
+    int length=text.Length;
+    for(int i=0;i<length;i++)
+    {
+        if (text[i]==oldval) {result+=$"{newval}";}
+        else {result+=$"{text[i]}";}
+    }
+    
+
+    return result;
+}
